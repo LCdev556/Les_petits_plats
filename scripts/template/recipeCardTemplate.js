b041c9d525dd14d20f664072f8ebf1016c356299
LCdev556 <leo.colrob@outlook.fr>
2024-07-04 18:14:01 +0200

    Co-authored-by: Yassin Hammou Ouali <y.hammououali@gmail.com>

--- scripts/template/recipeCardTemplate.js
+++ scripts/template/recipeCardTemplate.js
@@ -1,43 +1,99 @@
 export function recipeTemplate(recipe){
 
-    const {name, image, ingredients, description } = recipe
+    const {id, name, image, ingredients, description, ustensils } = recipe;
 
-    const picture = `assets/picture/${image}`
+    const picture = `assets/picture/${image}`;
 
-    function getRecipeCard(){
+    function getRecipeCard() {
 
-    const card = document.createElement ('article');
+        const card = document.createElement ('article');
+        card.className = "card"
+        card.setAttribute('data-id', id);
 
-    const cardPicture = document.createElement('img');
-    cardPicture.setAttribute("src", picture);
+        const ingredientsString = ingredients.map(item => item.ingredient).join(' , ');
+        card.setAttribute('data-ingredients', ingredientsString);
+        
+        card.setAttribute('data-ustensils', ustensils)
 
-    const cardContent = document.createElement('div')
+        const cardPicture = document.createElement('img');
+        cardPicture.setAttribute("src", picture);
+        cardPicture.className = "card-img-top"
 
-    const recipeTitle = document.createElement('h2')
-    recipeTitle.textContent = name
+        const cardContent = document.createElement('div')
+        cardContent.className = "card-body"
 
-    const descriptionContainer = document.createElement('div')
+        const recipeTitle = document.createElement('h2')
+        recipeTitle.textContent = name
+        recipeTitle.className = "card-title font"
 
-    const descriptionTitle = document.createElement('h3')
-    descriptionTitle.textContent = "Recette"
+        const descriptionContainer = document.createElement('div')
 
-    const descriptionText = document.createElement('p')
-    descriptionText.textContent = description
+        const descriptionTitle = document.createElement('h3')
+        descriptionTitle.textContent = "Recette"
+        descriptionTitle.className = "description_titl"
 
-    const ingredientContainer = document.createElement('div')
+        const descriptionText = document.createElement('p')
+        descriptionText.className = "descriptionText"
+        descriptionText.textContent = description
+        
 
-    const ingredientListTitle = document.createElement('h3')
-    ingredientListTitle.textContent = "Ingrédients"
+        const ingredientContainer = document.createElement('div')
+        ingredientContainer.className = "caractéristiques"
+        
 
-    card.appendChild(cardPicture)
-    cardContent.appendChild(recipeTitle)
-    descriptionContainer.appendChild(descriptionTitle)
-    descriptionContainer.appendChild(descriptionText)
-    ingredientContainer.appendChild(ingredientListTitle)
-    cardContent.appendChild(descriptionContainer)
-    cardContent.appendChild(ingredientContainer)
-    card.appendChild(cardContent)
+        const ingredientListTitle = document.createElement('h3')
+        ingredientListTitle.textContent = "Ingrédients"
+        ingredientListTitle.className = "description_titl"
 
-    return(card);
+        
+
+        ingredients.forEach(Element => {
+            
+            const {quantity, unit, ingredient } = Element;
+
+            card.setAttribute('data-ingredient', ingredient)
+
+            const IngredientElement = document.createElement('div')
+            IngredientElement.className = "ingrédient"
+
+
+            const ingredientName = document.createElement('p')
+            ingredientName.textContent = Element.ingredient
+            ingredientName.className = "ingredient_name"
+
+            IngredientElement.appendChild(ingredientName)
+        
+            if (Element.quantity) {
+                const ingredientQuantityValue = document.createElement('p')
+                ingredientQuantityValue.textContent = quantity
+                ingredientQuantityValue.className = "quantity"
+        
+                IngredientElement.appendChild( ingredientQuantityValue)
+            }
+        
+            if (Element.unit) {
+                const ingredientQuantityUnitValue = document.createElement('p')
+                ingredientQuantityUnitValue.textContent = unit
+                ingredientQuantityUnitValue.className = "quantity"
+        
+                IngredientElement.appendChild( ingredientQuantityUnitValue)
+            }
+            
+            ingredientContainer.appendChild(IngredientElement)
+        
+        });
+
+        card.appendChild(cardPicture)
+        cardContent.appendChild(recipeTitle)
+        descriptionContainer.appendChild(descriptionTitle)
+        descriptionContainer.appendChild(descriptionText)
+        cardContent.appendChild(descriptionContainer)
+        cardContent.appendChild(ingredientListTitle)
+        cardContent.appendChild(ingredientContainer)
+        card.appendChild(cardContent)
+
+        return(card);
+    }
+    return {getRecipeCard}
 }
-}
+
--- scripts/utils/recipeDataDisplay.js
+++ scripts/utils/recipeDataDisplay.js
@@ -3,9 +3,13 @@ import { recipeTemplate } from "../template/recipeCardTemplate.js";
 const recipesSection = document.querySelector('recipe_section')
 
 export async function displayRecipeData(recipes){
+
+    const recipesSection = document.querySelector('.recipe_section')
+
     recipes.forEach((recipe) => {
         const recipeCardModel = recipeTemplate(recipe);
-        const recipeCardDOM = recipeCardModel.getRecipeCard()
+        const recipeCardDOM = recipeCardModel.getRecipeCard();
+       
         
         recipesSection.appendChild(recipeCardDOM);
     });
